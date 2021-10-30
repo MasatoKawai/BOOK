7b73828487306b765834d273705ad10b7bf6432d
河合雅斗 <masato@Masatos-m1.local>
2021-10-30 16:06:03 +0900

    check it shows picture user post and works

--- データ収集からWebアプリ開発まで実践で学ぶ機械学習活用ガイド/chapter5/WebApp/app.py
+++ データ収集からWebアプリ開発まで実践で学ぶ機械学習活用ガイド/chapter5/WebApp/app.py
@@ -1,15 +1,41 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
+import os
+
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = './static/images/upload'
+# アップロードを許可する拡張子の設定
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 # appという名前でFlaskオブジェクトをインスタンス化
 app = Flask(__name__)
+# flashメッセージ用に任意のキーを設定
+app.secret_key = 'flash_key'
+
+# アップロードされたファイルの拡張子をチェック
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 # rootディレクトリにアクセスした場合の挙動
-
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method=='POST':
+      if 'file' not in request.files:
+        flash('ファイルを選択してください')
+        return render_template('index.html')
+      
+      file = request.files['file']
+      if not allowed_file(file.filename):
+        flash('拡張子はpng, jpg, jpegのみ使用可能です')
+        return render_template('index.html')
+      else:
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(UPLOAD_FOLDER, filename)
+        file.save(file_path)
+        return render_template('display_img.html', file_path=file_path)
+    else:
+      return render_template('index.html')
 
 
 # メインで実行される関数
